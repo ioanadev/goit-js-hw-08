00dea5966f05f0858ff973759dc88d44630e5935
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-05-07 23:13:34 +0300

    am instalat simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
 console.log(galleryItems);
+let gallery = document.querySelector('.gallery');
+console.log(gallery);
+galleryItems.forEach(item => {
+  //itereaza matricea galleryItems si creaza markup pentru fiecare element
+  const galleryMarkup = `<li class = "gallery__item">
+        <a class = "gallery__link" href = "${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+        </li >`;
+  gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+});
+new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+  showCounter: false,
+  scrollZoomFactor: 0.1,
+});
