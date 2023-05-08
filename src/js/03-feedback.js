a0a8e8bc7d9fc5f5140cf3449adcadc9f6921c8d
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-05-08 20:17:23 +0300

    am rezolvat exercitiul 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const feedbackForm = 'feedback-form-state';
+console.log(form);
+
+function saveStateToLocalStorage() {
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageTextarea = form.querySelector('textarea[name="message"]');
+
+  const state = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(state);
+  localStorage.setItem(feedbackForm, JSON.stringify(state));
+}
+
+form.addEventListener(
+  'input',
+  throttle(saveStateToLocalStorage, 500, { leading: false, trailing: true })
+);
+function loadStateFromLocalStorage() {
+  const state = localStorage.getItem(feedbackForm);
+
+  if (state) {
+    const { email, message } = JSON.parse(state);
+
+    const emailInput = form.querySelector('input[name="email"]');
+    const messageTextarea = form.querySelector('textarea[name="message"]');
+
+    emailInput.value = email;
+    messageTextarea.value = message;
+  }
+}
+
+loadStateFromLocalStorage();
+form.addEventListener('submit', event => {
+  event.preventDefault;
+});
