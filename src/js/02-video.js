c4636030b0dec9a5193cd38ec7cba7940981b13a
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-05-08 12:48:25 +0300

    am terminat ex 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.ready().then(() => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+
+  player.on(
+    'timeupdate',
+    throttle(() => {
+      const currentTime = Math.floor(player.getCurrentTime());
+      localStorage.setItem('videoplayer-current-time', currentTime);
+    }, 1000)
+  );
+});
