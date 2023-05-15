300f4fdf5d3d7f5dea2ca8a34c8ab6e9dfef8d62
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-05-15 21:27:54 +0300

    am rezolvat exercitiul 3

--- src/js/tema 9/03-promises.js
+++ src/js/tema 9/03-promises.js
@@ -1,8 +1,50 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector('.form');
+//console.log(form);
+const delayInput = document.querySelector('input[name="delay"]');
+console.log(delayInput);
+const stepInput = document.querySelector('input[name="step"]');
+//console.log(step);
+const amountInput = document.querySelector('input[name="amount"]');
+//console.log(amount);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(position);
+      }, delay);
+    });
   } else {
-    // Reject
+    return new Promise(reject => {
+      setTimeout(() => {
+        reject(position);
+      }, delay);
+    });
   }
 }
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = amountInput.value;
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const currentDelay = delay + i * step;
+    createPromise(position, currentDelay)
+      .then(position => {
+        //console.log(`Fulfilled promise ${position} in ${currentDelay}ms`);
+        var successMessage = `Fulfilled promise ${position} in ${currentDelay}ms`;
+        Notify.success(successMessage);
+      })
+
+      .catch(position => {
+        // console.log(`Rejected promise ${position} in ${currentDelay}ms`);
+        var feilureMessage = `Rejected promise ${position} in ${currentDelay}ms`;
+        Notify.failure(feilureMessage);
+      });
+  }
+});
